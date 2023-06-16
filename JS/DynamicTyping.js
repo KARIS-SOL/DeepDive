c70f364e00a61c08a58992af9f204e6516cee08b
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-06-16 19:03:10 +0900

    호이스팅

---
+++ JS/DynamicTyping.js
@@ -0,0 +1,21 @@
+let foo;
+
+console.log(typeof foo); // undefined
+
+foo = null;
+console.log(typeof foo); // object
+
+foo = {};
+console.log(typeof foo); // object
+
+foo = 3;
+console.log(typeof foo); // number
+
+foo = 3.14;
+console.log(typeof foo); // number
+
+foo = "Hi";
+console.log(typeof foo); // string
+
+foo = true;
+console.log(typeof foo); // boolean
