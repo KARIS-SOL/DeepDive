375de3e908234d5e536ff8fa1014c472069c1c72
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-06-26 21:04:23 +0900

    DOM 공부

---
+++ JS/ForOf.js
@@ -0,0 +1,22 @@
+const array = [1, 2, 3];
+array.name = "my array";
+
+for (const value of array) {
+  console.log(value);
+}
+
+/*
+1
+2
+3
+*/
+
+for (const [index, value] of array.entries()) {
+  console.log(index, value);
+}
+
+/*
+0 1
+1 2
+2 3
+*/
