ca1832c087dc4af83635d8dca8eb043883e1cac6
JIN SOL BAIK <121254493+KARIS-SOL@users.noreply.github.com>
2023-07-11 22:50:29 +0900

    boolean

--- JS/ExplicitCoercion.js
+++ JS/ExplicitCoercion.js
@@ -80,3 +80,19 @@ console.log(Boolean({})); // true
 console.log(Boolean([])); // true
 
 // 2. ! 부정 논리 연산자를 두번 사용하는 방법
+// 문자열 타입 => 불리언 타입
+console.log(!!"x"); // true
+console.log(!!""); // false
+console.log(!!"false"); // true
+// 숫자 타입 => 불리언 타입
+console.log(!!0); // false
+console.log(!!1); // true
+console.log(!!NaN); // false
+console.log(!!Infinity); // true
+// null 타입 => 불리언 타입
+console.log(!!null); // false
+// undefined 타입 => 불리언 타입
+console.log(!!undefined); // false
+// 객체 타입 => 불리언 타입
+console.log(!!{}); // true
+console.log(!![]); // true
